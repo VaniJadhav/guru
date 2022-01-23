c529781fc0b5e481cc172116d16287b1c532b7a1
vanijadhav <vanijadhav2010@gmail.com>
2022-01-23 20:15:55 +0530

    changed the chrome path

--- src/test/java/com/guru/testcases/LoginTestCase1.java
+++ src/test/java/com/guru/testcases/LoginTestCase1.java
@@ -3,17 +3,26 @@ package com.guru.testcases;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class LoginTestCase1 {
 	
-	
+	WebDriver driver;
 	@BeforeClass
 	public void setUP()
 	{
-		System.setProperty("WebDriver.chromedriver.driver", "C:\\Users\\vanij\\Downloads\\Automation classes\\guru\\drivers\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.gamil.com/");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\vanij\\Downloads\\Work\\chromedriver.exe");
+		
+		 driver= new ChromeDriver();
+		
+
 		
 	}
 
+	@Test
+	public void login()
+	{
+		driver.get("http://www.gamil.com/");
+	}
+	
 }
